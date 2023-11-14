11e259d159468a064be1c93c7faf4c2cc2cb306d
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2023-11-14 15:04:11 +0100

    Update tests to make use of snapshots (#290)

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,322 +1,94 @@
 """Test for HomeWizard Energy Models."""
 
 import json
-from datetime import datetime
 
-from homewizard_energy import Data, Decryption, Device, ExternalDevice, State, System
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from homewizard_energy import Data, Decryption, Device, State, System
 
 from . import load_fixtures
 
-
-def test_device_with_expected_parameters():
-    """TODO."""
-    device: Device = Device.from_dict(json.loads(load_fixtures("device.json")))
-    assert device
-    assert device.product_type == "HWE-P1"
-    assert device.product_name == "P1 Meter"
-    assert device.serial == "3c39e7aabbcc"
-    assert device.firmware_version == "2.11"
-    assert device.api_version == "v1"
+pytestmark = [pytest.mark.asyncio]
 
 
-def test_device_with_new_api_version():
-    """TODO."""
-    device: Device = Device.from_dict(
-        json.loads(load_fixtures("device_invalid_api.json"))
-    )
-    assert device
-    assert device.product_type == "HWE-P1"
-    assert device.product_name == "P1 Meter"
-    assert device.serial == "3c39e7aabbcc"
-    assert device.firmware_version == "2.11"
-    assert device.api_version == "v2"
-
-
-def test_device_with_unknown_datafields():
-    """TODO."""
-    device: Device = Device.from_dict(
-        json.loads(load_fixtures("device_extra_parameters.json"))
-    )
-    assert device
-    assert device.product_type == "HWE-P1"
-    assert device.product_name == "P1 Meter"
-    assert device.serial == "3c39e7aabbcc"
-    assert device.firmware_version == "2.11"
-    assert device.api_version == "v1"
-
-
-def test_data_p1():
-    """TODO."""
-    data: Data = Data.from_dict(json.loads(load_fixtures("data_p1.json")))
-
+@pytest.mark.parametrize(
+    "fixture",
+    [
+        "device.json",
+        "device_energysocket.json",
+        "device_extra_parameters.json",
+        "device_invalid_api.json",
+    ],
+)
+async def test_device(fixture: str, snapshot: SnapshotAssertion):
+    """Test Device model."""
+    data = Device.from_dict(json.loads(load_fixtures(fixture)))
     assert data
-    assert data.smr_version == 50
-    assert data.meter_model == "ISKRA  2M550T-101"
-    assert data.wifi_ssid == "My Wi-Fi"
-    assert data.wifi_strength == 100
-    assert data.total_energy_import_t1_kwh == 10830.511
-    assert data.total_energy_import_t2_kwh == 2948.827
-    assert data.total_energy_export_t1_kwh == 1285.951
-    assert data.total_energy_export_t2_kwh == 2876.51
-    assert data.active_power_w == -543
-    assert data.active_power_l1_w == -676
-    assert data.active_power_l2_w == 133
-    assert data.active_power_l3_w == 0
-    assert data.total_gas_m3 == 2569.646
-    assert data.gas_timestamp == datetime(2021, 6, 6, 14, 0, 10)
+
+    assert snapshot == data
 
 
-def test_data_p1_no_gas():
-    """TODO."""
-    data: Data = Data.from_dict(json.loads(load_fixtures("data_p1_no_gas.json")))
-
+@pytest.mark.parametrize(
+    "fixture",
+    [
+        "data_energysocket.json",
+        "data_kwh_single_phase.json",
+        "data_kwh_three_phase.json",
+        "data_p1_full.json",
+        "data_p1_no_gas.json",
+        "data_p1_single_phase.json",
+        "data_p1.json",
+        "data_watermeter.json",
+    ],
+)
+async def test_data(fixture: str, snapshot: SnapshotAssertion):
+    """Test Data model."""
+    data = Data.from_dict(json.loads(load_fixtures(fixture)))
     assert data
-    assert data.smr_version == 50
-    assert data.meter_model == "ISKRA  2M550T-101"
-    assert data.wifi_ssid == "My Wi-Fi"
-    assert data.wifi_strength == 100
-    assert data.total_energy_import_t1_kwh == 10830.511
-    assert data.total_energy_import_t2_kwh == 2948.827
-    assert data.total_energy_export_t1_kwh == 1285.951
-    assert data.total_energy_export_t2_kwh == 2876.51
-    assert data.active_power_w == -543
-    assert data.active_power_l1_w == -676
-    assert data.active_power_l2_w == 133
-    assert data.active_power_l3_w == 0
-    assert data.total_gas_m3 is None
-    assert data.gas_timestamp is None
+
+    assert snapshot == data
 
 
-def test_data_p1_single_phase():
-    """TODO."""
-    data: Data = Data.from_dict(json.loads(load_fixtures("data_p1_single_phase.json")))
-
+@pytest.mark.parametrize(
+    "fixture",
+    [
+        "decryption.json",
+    ],
+)
+async def test_decryption(fixture: str, snapshot: SnapshotAssertion):
+    """Test Decryption model."""
+    data = Decryption.from_dict(json.loads(load_fixtures(fixture)))
     assert data
-    assert data.smr_version == 50
-    assert data.meter_model == "ISKRA  2M550T-101"
-    assert data.wifi_ssid == "My Wi-Fi"
-    assert data.wifi_strength == 100
-    assert data.total_energy_import_t1_kwh == 10830.511
-    assert data.total_energy_import_t2_kwh == 2948.827
-    assert data.total_energy_export_t1_kwh == 1285.951
-    assert data.total_energy_export_t2_kwh == 2876.51
-    assert data.active_power_w == -543
-    assert data.active_power_l1_w == -676
-    assert data.active_power_l2_w is None
-    assert data.active_power_l3_w is None
-    assert data.total_gas_m3 == 2569.646
-    assert data.gas_timestamp == datetime(2021, 6, 6, 14, 0, 10)
+
+    assert snapshot == data
 
 
-def test_data_p1_full():
-    """TODO."""
-    # pylint: disable=too-many-statements
-
-    data: Data = Data.from_dict(json.loads(load_fixtures("data_p1_full.json")))
-
+@pytest.mark.parametrize(
+    "fixture",
+    [
+        "state.json",
+        "state_switch_lock_and_brightness.json",
+    ],
+)
+async def test_state(fixture: str, snapshot: SnapshotAssertion):
+    """Test State model."""
+    data = State.from_dict(json.loads(load_fixtures(fixture)))
     assert data
-    assert data.wifi_ssid == "My Wi-Fi"
-    assert data.wifi_strength == 100
-    assert data.smr_version == 50
-    assert data.meter_model == "ISKRA  2M550T-101"
-    assert data.unique_meter_id == "NGGYU"
-    assert data.active_tariff == 2
-    assert data.total_energy_import_kwh == 13779.338
-    assert data.total_energy_import_t1_kwh == 10830.511
-    assert data.total_energy_import_t2_kwh == 2948.827
-    assert data.total_energy_export_kwh == 0
-    assert data.total_energy_export_t1_kwh == 0
-    assert data.total_energy_export_t2_kwh == 0
-    assert data.active_power_w == -543
-    assert data.active_power_l1_w == -676
-    assert data.active_power_l2_w == 133
-    assert data.active_power_l3_w == 0
-    assert data.active_current_l1_a == -4
-    assert data.active_current_l2_a == 2
-    assert data.active_current_l3_a == 0
-    assert data.active_frequency_hz == 50
-    assert data.voltage_sag_l1_count == 1
-    assert data.voltage_sag_l2_count == 1
-    assert data.voltage_sag_l3_count == 0
-    assert data.voltage_swell_l1_count == 0
-    assert data.voltage_swell_l2_count == 0
-    assert data.voltage_swell_l3_count == 0
-    assert data.any_power_fail_count == 4
-    assert data.long_power_fail_count == 5
-    assert data.total_gas_m3 == 2569.646
-    assert data.gas_timestamp == datetime(2021, 6, 6, 14, 0, 10)
-    assert data.gas_unique_id == "NGLYD"
-    assert data.active_power_average_w == 123.000
-    assert data.monthly_power_peak_w == 1111.000
-    assert data.monthly_power_peak_timestamp == datetime(2023, 1, 1, 8, 0, 10)
 
-    assert len(data.external_devices) == 5
-
-    device = data.external_devices["NGLYD"]
-    assert device.unique_id == "NGLYD"
-    assert device.meter_type == ExternalDevice.DeviceType.GAS_METER
-    assert device.value == 111.111
-    assert device.unit == "m3"
-    assert device.timestamp == datetime(2021, 6, 6, 14, 0, 10)
-
-    device = data.external_devices["NGRAADY"]
-    assert device.unique_id == "NGRAADY"
-    assert device.meter_type == ExternalDevice.DeviceType.HEAT_METER
-    assert device.value == 222.222
-    assert device.unit == "m3"
-    assert device.timestamp == datetime(2021, 6, 6, 14, 0, 10)
-
-    device = data.external_devices["NGMYC"]
-    assert device.unique_id == "NGMYC"
-    assert device.meter_type == ExternalDevice.DeviceType.WARM_WATER_METER
-    assert device.value == 333.333
-    assert device.unit == "m3"
-    assert device.timestamp == datetime(2021, 6, 6, 14, 0, 10)
-
-    device = data.external_devices["NGSG"]
-    assert device.unique_id == "NGSG"
-    assert device.meter_type == ExternalDevice.DeviceType.WATER_METER
-    assert device.value == 444.444
-    assert device.unit == "m3"
-    assert device.timestamp == datetime(2021, 6, 6, 14, 0, 10)
-
-    device = data.external_devices["NGTALAHY"]
-    assert device.unique_id == "NGTALAHY"
-    assert device.meter_type == ExternalDevice.DeviceType.INLET_HEAT_METER
-    assert device.value == 555.555
-    assert device.unit == "m3"
-    assert device.timestamp == datetime(2021, 6, 6, 14, 0, 10)
+    assert snapshot == data
 
 
-def test_unique_id_is_not_parsed_when_invalid():
-    """TODO."""
-    # pylint: disable=too-many-statements
-
-    data: Data = Data.from_dict({"unique_id": "not_parsable"})
-
+@pytest.mark.parametrize(
+    "fixture",
+    [
+        "system_cloud_disabled.json",
+        "system_cloud_enabled.json",
+    ],
+)
+async def test_system(fixture: str, snapshot: SnapshotAssertion):
+    """Test System model."""
+    data = System.from_dict(json.loads(load_fixtures(fixture)))
     assert data
-    assert data.unique_meter_id == "not_parsable"
 
-
-def test_data_watermeter():
-    """TODO."""
-    data: Data = Data.from_dict(json.loads(load_fixtures("data_watermeter.json")))
-
-    assert data
-    assert data.wifi_ssid == "My Wi-Fi"
-    assert data.wifi_strength == 100
-    assert data.active_liter_lpm == 13.12
-    assert data.total_liter_m3 == 8129.123
-
-
-def test_data_kwh_single_phase():
-    """TODO."""
-
-    data: Data = Data.from_dict(json.loads(load_fixtures("data_kwh_single_phase.json")))
-
-    assert data
-    assert data.smr_version is None
-    assert data.meter_model is None
-    assert data.wifi_ssid == "My Wi-Fi"
-    assert data.wifi_strength == 100
-    assert data.total_energy_import_t1_kwh == 10830.511
-    assert data.total_energy_import_t2_kwh is None
-    assert data.total_energy_export_t1_kwh == 2948.827
-    assert data.total_energy_export_t2_kwh is None
-    assert data.active_power_w == 640
-    assert data.active_power_l1_w == 640
-    assert data.active_power_l2_w is None
-    assert data.active_power_l3_w is None
-    assert data.total_gas_m3 is None
-    assert data.gas_timestamp is None
-
-
-def test_data_kwh_three_phase():
-    """TODO."""
-    data: Data = Data.from_dict(json.loads(load_fixtures("data_kwh_three_phase.json")))
-
-    assert data
-    assert data.smr_version is None
-    assert data.meter_model is None
-    assert data.wifi_ssid == "My Wi-Fi"
-    assert data.wifi_strength == 100
-    assert data.total_energy_import_t1_kwh == 10830.511
-    assert data.total_energy_import_t2_kwh is None
-    assert data.total_energy_export_t1_kwh == 2948.827
-    assert data.total_energy_export_t2_kwh is None
-    assert data.active_power_w == -543
-    assert data.active_power_l1_w == -676
-    assert data.active_power_l2_w == 133
-    assert data.active_power_l3_w == 0
-    assert data.total_gas_m3 is None
-    assert data.gas_timestamp is None
-
-
-def test_data_energysocket():
-    """TODO."""
-    data: Data = Data.from_dict(json.loads(load_fixtures("data_energysocket.json")))
-
-    assert data
-    assert data.smr_version is None
-    assert data.meter_model is None
-    assert data.wifi_ssid == "My Wi-Fi"
-    assert data.wifi_strength == 100
-    assert data.total_energy_import_kwh == 10830.511
-    assert data.total_energy_import_t1_kwh == 10830.511
-    assert data.total_energy_import_t2_kwh is None
-    assert data.total_energy_export_kwh == 2948.827
-    assert data.total_energy_export_t1_kwh == 2948.827
-    assert data.total_energy_export_t2_kwh is None
-    assert data.active_power_w == 123
-    assert data.active_power_l1_w == 123
-    assert data.active_power_l2_w is None
-    assert data.active_power_l3_w is None
-    assert data.total_gas_m3 is None
-    assert data.gas_timestamp is None
-
-
-def test_state():
-    """TODO."""
-    state: State = State.from_dict(json.loads(load_fixtures("state.json")))
-
-    assert state
-    assert not state.power_on
-    assert not state.switch_lock
-    assert state.brightness == 255
-
-    state: State = State.from_dict(
-        json.loads(load_fixtures("state_switch_lock_and_brightness.json"))
-    )
-
-    assert state
-    assert state.power_on
-    assert state.switch_lock
-    assert state.brightness == 10
-
-
-def test_system():
-    """TODO."""
-    system: System = System.from_dict(
-        json.loads(load_fixtures("system_cloud_enabled.json"))
-    )
-
-    assert system
-    assert system.cloud_enabled
-
-    system: System = System.from_dict(
-        json.loads(load_fixtures("system_cloud_disabled.json"))
-    )
-
-    assert system
-    assert not system.cloud_enabled
-
-
-def test_decryption():
-    """TODO."""
-    decryption: Decryption = Decryption.from_dict(
-        json.loads(load_fixtures("decryption.json"))
-    )
-
-    assert decryption
-    assert decryption.key_set
-    assert decryption.aad_set
+    assert snapshot == data
