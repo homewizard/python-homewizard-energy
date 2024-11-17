e9443e7655265d7ac6974fd0b072810aea553135
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-11-17 13:59:19 +0100

    Implement mimimum WebSocket API

--- homewizard_energy/v2/const.py
+++ homewizard_energy/v2/const.py
@@ -1,6 +1,16 @@
 """Constants for HomeWizard Energy."""
 
-SUPPORTED_API_VERSION = "v1"
+from enum import StrEnum
+
+SUPPORTED_API_VERSION = "2.0.0"
 
 SUPPORTS_STATE = ["HWE-SKT"]
 SUPPORTS_IDENTIFY = ["HWE-SKT", "HWE-P1", "HWE-WTR"]
+
+
+class WebsocketTopic(StrEnum):
+    """Websocket topics."""
+
+    DEVICE = "device"
+    MEASUREMENT = "measurement"
+    SYSTEM = "system"
