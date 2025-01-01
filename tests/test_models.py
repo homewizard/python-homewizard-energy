5f87b191e9a089a52ad7931842039419478a21ad
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2025-01-01 12:49:45 +0100

    Remove Product model and simplify model name conversion (#449)

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,8 @@
 """Test the helper functions."""
 
 import pytest
-from syrupy.assertion import SnapshotAssertion
 
-from homewizard_energy.models import Product, get_verification_hostname
+from homewizard_energy.models import get_verification_hostname
 
 pytestmark = [pytest.mark.asyncio]
 
@@ -34,79 +33,3 @@ async def test_get_verification_hostname_raises_value_error():
     """Test if get_verification_hostname raises a ValueError for an unsupported model."""
     with pytest.raises(ValueError):
         get_verification_hostname("unsupported", "1234567890")
-
-
-@pytest.mark.parametrize(
-    ("model"),
-    [
-        ("HWE-P1"),
-        ("HWE-SKT"),
-        ("HWE-WTR"),
-        ("HWE-KWH1"),
-        ("HWE-KWH3"),
-        ("SDM230-wifi"),
-        ("SDM630-wifi"),
-        ("HWE-BAT"),
-    ],
-)
-async def test_known_products(model: str, snapshot: SnapshotAssertion):
-    """Test known products."""
-
-    product = Product.from_type(model)
-    assert product is not None
-    assert snapshot == product
-
-
-@pytest.mark.parametrize(
-    ("model"),
-    [
-        ("HWE-P1"),
-        ("HWE-SKT"),
-        ("HWE-WTR"),
-        ("HWE-KWH1"),
-        ("HWE-KWH3"),
-        ("SDM230-wifi"),
-        ("SDM630-wifi"),
-        ("HWE-BAT"),
-    ],
-)
-async def test_known_product_strings(model: str, snapshot: SnapshotAssertion):
-    """Test generating product strings."""
-
-    product = Product.from_type(model)
-    assert product is not None
-    assert snapshot == str(product)
-
-
-@pytest.mark.parametrize(
-    ("model"),
-    [
-        ("HWE-P2"),
-        (None),
-        ("WTR"),
-        (""),
-    ],
-)
-async def test_unknown_or_invalid_products(model: str, snapshot: SnapshotAssertion):
-    """Test unknown or invalid products types."""
-
-    product = Product.from_type(model)
-    assert product is None
-    assert snapshot == product
-
-
-@pytest.mark.parametrize(
-    ("lhs", "rhs", "value"),
-    [
-        ("HWE-P1", "HWE-P1", True),
-        ("HWE-P1", "HWE-SKT", False),
-        ("HWE-P1", None, False),
-        ("HWE-P1", "", False),
-    ],
-)
-async def test_comparison_between_products(lhs: str, rhs: str, value: bool):
-    """Test comparison between products."""
-
-    lhs = Product.from_type(lhs)
-    rhs = Product.from_type(rhs)
-    assert (lhs == rhs) == value
