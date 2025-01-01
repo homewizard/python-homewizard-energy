be3632e6c2429770faa904f2289f83242adfbc47
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2025-01-01 12:44:03 +0100

    Remove Product model and simplify model name conversion

--- homewizard_energy/const.py
+++ homewizard_energy/const.py
@@ -3,3 +3,26 @@
 import logging
 
 LOGGER = logging.getLogger(__name__)
+
+MODEL_TO_ID = {
+    "HWE-P1": "p1dongle",
+    "HWE-SKT": "energysocket",
+    "HWE-WTR": "watermeter",
+    "HWE-DSP": "display",
+    "HWE-KWH1": "energymeter",
+    "SDM230-wifi": "energymeter",
+    "HWE-KWH3": "energymeter",
+    "SDM630-wifi": "energymeter",
+    "HWE-BAT": "battery",
+}
+
+MODEL_TO_NAME = {
+    "HWE-P1": "Wi-Fi P1 Meter",
+    "HWE-SKT": "Wi-Fi Energy Socket",
+    "HWE-WTR": "Wi-Fi Watermeter",
+    "HWE-KWH1": "Wi-Fi kWh Meter 1-phase",
+    "HWE-KWH3": "Wi-Fi kWh Meter 3-phase",
+    "SDM230-wifi": "Wi-Fi kWh Meter 1-phase",
+    "SDM630-wifi": "Wi-Fi kWh Meter 3-phase",
+    "HWE-BAT": "Plug-In Battery",
+}
--- homewizard_energy/models.py
+++ homewizard_energy/models.py
@@ -11,7 +11,7 @@ from mashumaro.config import BaseConfig
 from mashumaro.exceptions import MissingField
 from mashumaro.mixins.orjson import DataClassORJSONMixin
 
-from .const import LOGGER
+from .const import LOGGER, MODEL_TO_ID, MODEL_TO_NAME
 
 
 class BaseModel(DataClassORJSONMixin):
@@ -25,36 +25,23 @@ class BaseModel(DataClassORJSONMixin):
         omit_none = True
 
 
-MODELS = {
-    "HWE-P1": "p1dongle",
-    "HWE-SKT": "energysocket",
-    "HWE-WTR": "watermeter",
-    "HWE-DSP": "display",
-    "HWE-KWH1": "energymeter",
-    "SDM230-wifi": "energymeter",
-    "HWE-KWH3": "energymeter",
-    "SDM630-wifi": "energymeter",
-    "HWE-BAT": "battery",
-}
-
-
 def get_verification_hostname(model: str, serial_number: str) -> str:
     """Helper method to convert device model and serial to identifier
 
     The identifier is used to verify the device in the HomeWizard Energy API via HTTPS.
     """
 
-    if model not in MODELS:
+    if model not in MODEL_TO_ID:
         raise ValueError(f"Unsupported model: {model}")
 
-    return f"appliance/{MODELS[model]}/{serial_number}"
+    return f"appliance/{MODEL_TO_ID[model]}/{serial_number}"
 
 
 @dataclass(kw_only=True)
 class Device(BaseModel):
     """Represent Device config."""
 
-    product: Product | None = None
+    model_name: str | None = None
     id: str | None = None
 
     product_name: str = field()
@@ -68,7 +55,7 @@ class Device(BaseModel):
         """Post deserialize hook for Device object."""
         _ = cls  # Unused
 
-        obj.product = Product.from_type(obj.product_type)
+        obj.model_name = MODEL_TO_NAME.get(obj.product_type)
         obj.id = get_verification_hostname(obj.product_type, obj.serial)
         return obj
 
@@ -431,102 +418,6 @@ class ExternalDevice(BaseModel):
     timestamp: datetime = field(
         default=None, metadata={"deserialize": lambda x: Measurement.to_datetime(x)}
     )
-
-
-@dataclass(frozen=True)
-class Product:
-    """Represent a product."""
-
-    model: str
-    name: str | None
-    url: str | None
-    description: str | None
-
-    def __str__(self):
-        """Return a string representation of the product."""
-        return f"HomeWizard {self.name} - {self.model}"
-
-    def __hash__(self):
-        """Hash a product for unit-test snapshots."""
-        return hash((self.model, self.name, self.url, self.description))
-
-    def __eq__(self, other):
-        """Compare two products."""
-        if isinstance(other, Product):
-            return (
-                self.model == other.model
-                and self.name == other.name
-                and self.url == other.url
-                and self.description == other.description
-            )
-        return False
-
-    @staticmethod
-    def from_type(product_type: str, _: str | None = None) -> Product | None:
-        """Return a Product object from a type.
-
-        :param type: The type of the product.
-        :param locale: The locale to use for the description. Currently ignored. Should be an ISO 639-1 language code.
-        """
-
-        # pylint: disable=unused-argument
-
-        for product in PRODUCTS:
-            if product.model == product_type:
-                return product
-        return None
-
-
-PRODUCTS = {
-    Product(
-        "HWE-P1",
-        "Wi-Fi P1 Meter",
-        "https://www.homewizard.com/p1-meter/",
-        "The HomeWizard P1 Meter gives you detailed insight in your electricity-, gas consumption and solar surplus.",
-    ),
-    Product(
-        "HWE-SKT",
-        "Wi-Fi Energy Socket",
-        "https://www.homewizard.com/energy-socket/",
-        "Measure and switch every device.",
-    ),
-    Product(
-        "HWE-WTR",
-        "Wi-Fi Watermeter",
-        "https://www.homewizard.com/watermeter/",
-        "Real-time water consumption insights",
-    ),
-    Product(
-        "HWE-KWH1",
-        "Wi-Fi kWh Meter 1-phase",
-        "https://www.homewizard.com/kwh-meter/",
-        "Measure solar panels, car chargers and more.",
-    ),
-    Product(
-        "HWE-KWH3",
-        "Wi-Fi kWh Meter 3-phase",
-        "https://www.homewizard.com/kwh-meter/",
-        "Measure solar panels, car chargers and more.",
-    ),
-    Product(
-        "SDM230-wifi",
-        "Wi-Fi kWh Meter 1-phase",
-        "https://www.homewizard.com/kwh-meter/",
-        "Measure solar panels, car chargers and more.",
-    ),
-    Product(
-        "SDM630-wifi",
-        "Wi-Fi kWh Meter 3-phase",
-        "https://www.homewizard.com/kwh-meter/",
-        "Measure solar panels, car chargers and more.",
-    ),
-    Product(
-        "HWE-BAT",
-        "Plug-In Battery",
-        "https://www.homewizard.com/plug-in-battery/",
-        "Solar energy, day and night.",
-    ),
-}
 
 
 @dataclass(kw_only=True)
